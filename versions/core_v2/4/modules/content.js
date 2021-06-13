d419a1a023f4d833344be8e16479afbcedf4836d
superwibr <superwibr@gmail.com>
2021-06-13 11:50:55 -0400

    content

--- versions/core_v2/4/modules/content.js
+++ versions/core_v2/4/modules/content.js
@@ -2,7 +2,7 @@ let head, overlay;
 
 (async function(){
 	let template = async function(file){
-		return await fetch(`https://raw.githubusercontent.com/uncannyorange/DISSENSION/main/versions/core_v2/4/content/${file}.html`).then(res => res.text())
+		return await fetch(`https://uncannyorange.github.io/DISSENSION/versions/core_v2/4/content/${file}.html`).then(res => res.text())
 	}
 
 	head = await template("head");
