61d9972dd45bf7c520122be1f0c1bb2077e96e04
superwibr <theuncannyorange@gmail.com>
2021-12-11 18:41:11 -0500

    named request requirement

--- versions/client_v2/0.js
+++ versions/client_v2/0.js
@@ -11,22 +11,28 @@
 		document.querySelector('title').innerText = "DISS | Discord";
 
 		/* creating CSPdodge */
-		window.CSPDodge = function (url) {
+		window.CSPDodge = function (url, name = 'DISS23DEFAULTCOMMS') {
 			return new Promise(res => {
 				const win = window.open('https://uncannyorange.github.io/cspdodge.html', '_blank', 'width=1,height=1');
 				win.blur();
 				window.addEventListener('message', e => {
-					res(e.data);
+
+					res(e.data.resource);
 					win.close();
 				});
 
-				win.postMessage(url, '*');
+				win.postMessage({name, url}, '*');
 			});
 		};
+
+		/* getting core */
+		const sv = await CSPDodge('');
 	};
 
 	/* check for location */
-	if (location.href.startsWith('https://discord.com/channels/')) { /* is discord; run core */setup(); } else {
+	if (location.href.startsWith('https://discord.com/channels/')) {
+		/* is discord; run core */setup();
+	} else {
 		/* incorrect location; load discord */
 		location = "https://discord.com/channels/@me";
 	};
