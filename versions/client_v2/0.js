9566e96de5312a728ef78f6a81bac547cdcc6941
superwibr <theuncannyorange@gmail.com>
2022-01-03 19:47:28 -0500

    cspdodge now works

--- versions/client_v2/0.js
+++ versions/client_v2/0.js
@@ -6,9 +6,9 @@
  */
 
 (async dissension => {
-	const rawurl = "https://raw.githubusercontent.com/uncannyorange/DISSENSION/main/"
+	const rawurl = "https://raw.githubusercontent.com/uncannyorange/DISSENSION/main/";
 
-	const setup = function () {
+	const setup = async function () {
 		/* :> */
 		document.querySelector('title').innerText = "DISS | Discord";
 
@@ -17,26 +17,30 @@
 			return new Promise(res => {
 				const win = window.open('https://uncannyorange.github.io/cspdodge.html', '_blank', 'width=1,height=1');
 				win.blur();
-				window.addEventListener('message', e => {
-
-					res(e.data.resource);
+				window.addEventListener('message', function handler(e){
+					if (e.data.status == 'ready') { // wait for ready
+						win.postMessage({ name, url }, '*');
+						return;
+					};
+					console.log(e.data);
+					e.currentTarget.removeEventListener(e.type, handler); // remove event listner to avoid duplicates
+					res(e.data.resource); // resolve with resource
 					win.close();
 				});
-
-				win.postMessage({name, url}, '*');
 			});
 		};
 
 		/* getting core */
-		const sv = await CSPDodge(rawurl+'versions/core_v3/stable.txt', 'DISS23-STABLEVER'),
+		const sv = await CSPDodge(rawurl + 'versions/core_v3/stable.txt', 'DISS23-STABLEVER'),
 			core = await CSPDodge(`${rawurl}versions/core_v3/${sv}.js`);
-		
+
 		eval(core); /* eval's yucky but eh, it's necessary */
 	};
 
 	/* check for location */
 	if (location.href.startsWith('https://discord.com/channels/')) {
-		/* is discord; run core */setup();
+		/* is discord; run core */
+		setup();
 	} else {
 		/* incorrect location; load discord */
 		location = "https://discord.com/channels/@me";
