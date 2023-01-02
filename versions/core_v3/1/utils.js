585d1e6cc7dc2efbd09fec2c6c8d711d32847803
uncannyorange <theuncannyorange@gmail.com>
2023-01-02 10:34:22 -0500

    changed webpack, added ID getters

--- versions/core_v3/1/utils.js
+++ versions/core_v3/1/utils.js
@@ -1,8 +1,8 @@
 diss.utils = {
     log: message => console.log(`%c[DISSENSION] %c${message}`, `color:crimson;font-weight:bold`, ""),
     sleep: async ms => new Promise(res => setTimeout(res, ms)),
-    Webpack() {
-        if (this.cachedWebpack) return this.cachedWebpack;
+    cacheWebpack() {
+        if (diss.Webpack) return diss.Webpack;
         let webpackExports;
         if (window.webpackChunkdiscord_app != null) {
             const Ids = ['__extra_Id__'];
@@ -27,9 +27,9 @@ diss.utils = {
 
             return null;
         };
-        const findModuleByProps = (propNames) => findModule(module => propNames.every(prop => module[prop] !== undefined));
+        const findModuleByProps = propNames => findModule(module => propNames.every(prop => module[prop] !== undefined));
 
-        return this.cachedWebpack = { findModule, findModuleByProps };
+        return diss.Webpack = { findModule, findModuleByProps };
     },
     patch(obj, key, interceptor) {
         const ori = obj[key],
@@ -62,8 +62,18 @@ diss.utils = {
 
         return this.mergeDeep(target, ...sources);
     },
+
+    // current IDs
+    get channelId(){
+        return this.discordModules.SelectedChannelStore.getChannelId();
+    },
+    get guildId(){
+        return document.location.pathname.split('/')[2];
+    },
+
+    // notifs
     imsg(content, embeds = []) {
-        diss.modules.showMessage({ author: { username: "DISS" }, content, embeds });
+        diss.modules.showMessage(this.channelId, { author: { username: "DISS" }, content, embeds });
     }
 };
 diss.utils.log("Defined utilities (diss.utils)");
