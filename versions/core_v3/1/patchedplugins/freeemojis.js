1f47f1e667dd3aa4994a7084408b6e9ef6801507
uncannyorange <theuncannyorange@gmail.com>
2023-01-21 22:41:17 -0500

    it works

--- versions/core_v3/1/patchedplugins/freeemojis.js
+++ versions/core_v3/1/patchedplugins/freeemojis.js
@@ -1,3 +1,6 @@
+let getEmojiUnavailableReasonHook,
+    searchHook,
+    parseHook;
 diss.p.registerPlugin({
     name: "freeemojis",
     displayName: "this.storeFreeEmojis",
@@ -10,7 +13,7 @@ diss.p.registerPlugin({
 
         const { original_parse, original_getEmojiUnavailableReason } = this.store;
 
-        this.searchHook = function () {
+        searchHook = function () {
             let result = Discord.original_searchWithoutFetchingLatest.apply(this, arguments);
             console.log({ result, arguments })
             result.unlocked.push(...result.locked);
@@ -22,7 +25,7 @@ diss.p.registerPlugin({
             parseResult.content = parseResult.content.replace(`<${emoji.animated ? "a" : ""}:${emoji.originalName || emoji.name}:${emoji.id}>`, emoji.url.split("?")[0] + "?size=48");
         }
 
-        this.parseHook = function () {
+        parseHook = function () {
             let result = original_parse.apply(this, arguments);
 
             if (result.invalidEmojis.length !== 0) {
@@ -44,33 +47,30 @@ diss.p.registerPlugin({
             return result;
         };
 
-        this.getEmojiUnavailableReasonHook = function () {
+        getEmojiUnavailableReasonHook = function () {
             return null;
         }
     },
     disable() {
         if (!this.Initialized) return;
 
-        this.searchHook = this.store.original_searchWithoutFetchingLatest;
-        this.parseHook = this.store.original_parse;
-        this.getEmojiUnavailableReasonHook = this.store.original_getEmojiUnavailableReason;
+        searchHook = this.store.original_searchWithoutFetchingLatest;
+        parseHook = this.store.original_parse;
+        getEmojiUnavailableReasonHook = this.store.original_getEmojiUnavailableReason;
     },
 
     store: {},
     Initialized: false,
-    getEmojiUnavailableReasonHook: undefined,
-    searchHook: undefined,
-    parseHook: undefined,
     Init() {
-        this.searchHook = this.store.original_searchWithoutFetchingLatest = diss.dm.Emojis.searchWithoutFetchingLatest;
-        diss.dm.Emojis.searchWithoutFetchingLatest = function () { return this.searchHook.apply(this, arguments); };
 
-        this.parseHook = this.store.original_parse = diss.dm.MessageEmojiParser.parse;
-        diss.dm.MessageEmojiParser.parse = function () { return this.parseHook.apply(this, arguments); };
+        searchHook = this.store.original_searchWithoutFetchingLatest = diss.dm.Emojis.searchWithoutFetchingLatest;
+        diss.dm.Emojis.searchWithoutFetchingLatest = function () { return searchHook.apply(this, arguments); };
 
-        this.getEmojiUnavailableReasonHook = this.store.original_getEmojiUnavailableReason = diss.dm.EmojiPermissions.getEmojiUnavailableReason;
-        diss.dm.EmojiPermissions.getEmojiUnavailableReason = function () { return this.getEmojiUnavailableReasonHook.apply(this, arguments); };
+        parseHook = this.store.original_parse = diss.dm.MessageEmojiParser.parse;
+        diss.dm.MessageEmojiParser.parse = function () { return parseHook.apply(this, arguments); };
 
+        getEmojiUnavailableReasonHook = this.store.original_getEmojiUnavailableReason = diss.dm.EmojiPermissions.getEmojiUnavailableReason;
+        diss.dm.EmojiPermissions.getEmojiUnavailableReason = function () { return getEmojiUnavailableReasonHook.apply(this, arguments); };
 
         diss.u.log("[FreeEmojis] initialized");
         this.Initialized = true;
